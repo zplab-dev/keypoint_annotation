272159aa8f22d34029c347fd5317b7ad476f9e25
Nicolette Laird <n.laird@wustl.edu>
2020-10-14 10:11:36 -0500

    Update parameter_test_training.py

--- keypoint_annotation/training_scripts/parameter_test_training.py
+++ keypoint_annotation/training_scripts/parameter_test_training.py
@@ -4,26 +4,26 @@ from keypoint_annotation.training_scripts import gaussian_training_script
 from keypoint_annotation.training_scripts import vulva_classifier_script
 
 def parameter_test():
-	#cov_par = [25, 50, 100, 200]
-	cov_par = [100]
-	val_par = [1, 3, 100]
-	downscale = 1
-	image_shape = (960,96)
-	mask_error = False
+    #cov_par = [25, 50, 100, 200]
+    cov_par = [100]
+    val_par = [1, 3, 100]
+    downscale = 1
+    image_shape = (960,96)
+    mask_error = False
 
-	for covariate, max_val in itertools.product(cov_par, val_par):
-		print("Training with covariate {}, max_val {}".format(covariate, max_val))
-		image_size = (image_shape[0]/downscale, image_shape[1]/downscale)
-		project_name = '{}x{}_cov{}_max{}'.format(image_size[0], image_size[1],covariate, max_val)
-	    if mask_error:
-	        project_name += '_mask'
-	    #save_dir = './'+project_name
-	    if os_type == 'Darwin':
-	        save_dir = '/Volumes/lugia_array/Laird_Nicolette/deep_learning/keypoint_detection/new_api/production_dataloader_test/new_kp_maps/gaussian_kp/'+project_name
-	    elif os_type == 'Linux':
-	        save_dir = '/mnt/lugia_array/Laird_Nicolette/deep_learning/keypoint_detection/new_api/production_dataloader_test/new_kp_maps/gaussian_kp/'+project_name
-		#save_dir = gaussian_training_script.train_model(covariate, max_val, downscale, image_shape, mask_error)
-		vulva_classifier_script.train_vulva(save_dir, downscale, image_shape)
+    for covariate, max_val in itertools.product(cov_par, val_par):
+        print("Training with covariate {}, max_val {}".format(covariate, max_val))
+        image_size = (image_shape[0]/downscale, image_shape[1]/downscale)
+        project_name = '{}x{}_cov{}_max{}'.format(image_size[0], image_size[1],covariate, max_val)
+        if mask_error:
+            project_name += '_mask'
+        #save_dir = './'+project_name
+        if os_type == 'Darwin':
+            save_dir = '/Volumes/lugia_array/Laird_Nicolette/deep_learning/keypoint_detection/new_api/production_dataloader_test/new_kp_maps/gaussian_kp/'+project_name
+        elif os_type == 'Linux':
+            save_dir = '/mnt/lugia_array/Laird_Nicolette/deep_learning/keypoint_detection/new_api/production_dataloader_test/new_kp_maps/gaussian_kp/'+project_name
+        #save_dir = gaussian_training_script.train_model(covariate, max_val, downscale, image_shape, mask_error)
+        vulva_classifier_script.train_vulva(save_dir, downscale, image_shape)
 
 if __name__ == "__main__":
     parameter_test()
