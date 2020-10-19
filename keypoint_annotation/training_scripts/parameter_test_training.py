83a184e9845052651cfe2c2cbe27e0f9de1c6cdf
Nicolette Laird <n.laird@wustl.edu>
2020-10-19 13:17:35 -0500

    Update parameter_test_training.py

--- keypoint_annotation/training_scripts/parameter_test_training.py
+++ keypoint_annotation/training_scripts/parameter_test_training.py
@@ -3,24 +3,26 @@ import platform
 
 from keypoint_annotation.training_scripts import gaussian_training_script
 from keypoint_annotation.training_scripts import vulva_classifier_script
+from keypoint_annotation.training_scripts import sigmoid_training_script
 
 def parameter_test():
-    cov_par = [25, 50, 200]
+    #cov_par = [25, 50, 200]
     #cov_par = [100]
-    #val_par = [1, 3, 100]
-    val_par = [3]
+    val_par = [3, 100]
+    slope =[0.25]
+    #val_par = [3]
     downscale = 1
     image_shape = (960,96)
     mask_error = False
 
-    for covariate, max_val in itertools.product(cov_par, val_par):
+    for covariate, max_val in itertools.product(slope, val_par):
         print("Training with covariate {}, max_val {}".format(covariate, max_val))
         image_size = (int(image_shape[0]/downscale), int(image_shape[1]/downscale))
-        project_name = '{}x{}_cov{}_max{}'.format(image_size[0], image_size[1],covariate, max_val)
+        project_name = '{}x{}_slope{}_max{}'.format(image_size[0], image_size[1],covariate, max_val)
         if mask_error:
             project_name += '_mask'
         #save_dir = './'+project_name
-        save_dir = gaussian_training_script.train_model(covariate, max_val, downscale, image_shape, mask_error)
+        save_dir = sigmoid_training_script.train_model(covariate, max_val, downscale, image_shape, mask_error)
         vulva_classifier_script.train_vulva(save_dir, downscale, image_shape)
 
 if __name__ == "__main__":
