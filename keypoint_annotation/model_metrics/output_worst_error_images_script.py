0ae37d473731594b5e5fe1825c4b85a876073fcc
Nicolette Laird <n.laird@wustl.edu>
2020-10-21 22:52:56 -0500

    Update output_worst_error_images_script.py

--- keypoint_annotation/model_metrics/output_worst_error_images_script.py
+++ keypoint_annotation/model_metrics/output_worst_error_images_script.py
@@ -121,7 +121,6 @@ def plot_output_images(timepoint_list, kp_idx, save_name, model_paths, downscale
         keypoints = timepoint.annotations['keypoints']
 
         reflect = keypoints['vulva'][1] > 0
-        tensor_img = torch.flip(tensor_img, [3])
         tp_image = production_utils.get_worm_frame_image(timepoint, downscale=downscale, image_size=image_shape, reflect=reflect)
         extend_img = numpy.array([tp_image, tp_image, tp_image])
         gt_keypoints = timepoint.annotations['keypoints']
