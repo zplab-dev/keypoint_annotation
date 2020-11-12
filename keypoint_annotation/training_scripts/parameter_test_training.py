76815044adde47c18ecaf12f286502df98255bc7
Nicolette Laird <n.laird@wustl.edu>
2020-11-12 13:23:04 -0600

    changed training scripts to work on squidward

--- keypoint_annotation/training_scripts/parameter_test_training.py
+++ keypoint_annotation/training_scripts/parameter_test_training.py
@@ -16,15 +16,16 @@ def parameter_test():
     downscale = 1
     image_shape = (960,96)
     mask_error = False
+    epochs = 150
 
     for covariate, max_val in itertools.product(cov_par, val_par):
         print("Training with covariate {}, max_val {}".format(covariate, max_val))
         image_size = (int(image_shape[0]/downscale), int(image_shape[1]/downscale))
-        project_name = '{}x{}_cov{}_max{}'.format(image_size[0], image_size[1],covariate, max_val)
+        project_name = '{}x{}_cov{}_max{}_{}epochs'.format(image_size[0], image_size[1],covariate, max_val, epochs)
         if mask_error:
             project_name += '_mask'
         #save_dir = './'+project_name
-        save_dir = gaussian_training_script.train_model(covariate, max_val, downscale, image_shape, mask_error)
+        save_dir = gaussian_training_script.train_model(covariate, max_val, downscale, image_shape, mask_error, epochs)
         vulva_classifier_script.train_vulva(save_dir, downscale, image_shape)
 
 if __name__ == "__main__":
