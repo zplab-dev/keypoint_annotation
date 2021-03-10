cc3cde5cd37d45056eb2242ffde2a898c1f0a439
Nicolette Laird <n.laird@wustl.edu>
2021-03-09 23:12:51 -0600

    Update production_script.py

--- keypoint_annotation/production/production_script.py
+++ keypoint_annotation/production/production_script.py
@@ -37,6 +37,7 @@ if __name__ == "__main__":
     os_type = platform.system()
     parser = argparse.ArgumentParser()
     parser.add_argument('--sigmoid', default=False, action='store_true')
+    parser.add_argument('--dim1D', default=False, action='store_true')
     parser.add_argument('--epochs', type=int)
     parser.add_argument('exp_root', action='store', type=str)
     args = parser.parse_args()
@@ -54,6 +55,8 @@ if __name__ == "__main__":
     image_shape = (960,96)
     mask_error = False
     print(os_type)
+    covariate = 200
+    max_val = 3
     """try:
                     covariate, max_val, experiment_root = sys.argv[1], sys.argv[2], sys.argv[3]
                 except IndexError:
@@ -61,13 +64,20 @@ if __name__ == "__main__":
                     sys.exit(1)"""
 
     image_size = (int(image_shape[0]/downscale), int(image_shape[1]/downscale))
-    project_name = '{}x{}_cov{}_max{}'.format(image_size[0], image_size[1], covariate, max_val)
+    #project_name = '{}x{}_cov{}_max{}'.format(image_size[0], image_size[1], covariate, max_val)
+    if sigmoid:
+    	covariate = 1
+    	project_name = '{}x{}_slope{}_max{}'.format(image_size[0], image_size[1], covariate, max_val)
+    else:
+    	project_name = '{}x{}_cov{}_max{}'.format(image_size[0], image_size[1], covariate, max_val)
     if mask_error:
         project_name+='_mask'
     if os_type == 'Darwin':
-        model_path_root = '/Volumes/lugia_array/Laird_Nicolette/deep_learning/keypoint_detection/new_api/production_dataloader_test/new_api_960x96_cov100'
+        model_path_root = '/Volumes/lugia_array/Laird_Nicolette/deep_learning/keypoint_detection/new_api/production_dataloader_test/new_kp_maps/'
+        #model_path_root = '/Volumes/lugia_array/Laird_Nicolette/deep_learning/keypoint_detection/new_api/production_dataloader_test/new_api_960x96_cov100'
     elif os_type == 'Linux':
-        model_path_root = '/mnt/lugia_array/Laird_Nicolette/deep_learning/keypoint_detection/new_api/production_dataloader_test/new_api_960x96_cov100'
+       #model_path_root = '/mnt/lugia_array/Laird_Nicolette/deep_learning/keypoint_detection/new_api/production_dataloader_test/new_api_960x96_cov100'
+       model_path_root = '/Volumes/lugia_array/Laird_Nicolette/deep_learning/keypoint_detection/new_api/production_dataloader_test/new_kp_maps/'
 
     experiment = datamodel.Experiment(experiment_root)
     run_predictor(experiment, model_path_root, covariate, max_val, downscale, image_shape, mask_error, sigmoid, limited=False)
